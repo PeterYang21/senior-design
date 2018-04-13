e1a0e29f008c74596182e9b161d433dc4704b9f8
Yiming Yang <yyang70@u.rochester.edu>
2018-04-13 10:11:33 -0400

    Update testSensor.ino

--- arduino/testSensor.ino
+++ arduino/testSensor.ino
@@ -1,40 +1,54 @@
 #include <Servo.h>
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin3 = 9;
+const int echoPin3 = 5;
+const int trigPin5 = 11;
+const int echoPin5 = 12;
+const int speedPin = 14;
+const int steerPin = 15;
+const bool if_right = true;
+const bool if_straight = true;
 
 long duration;
 int distance;
-int value = 0; // set values you need to zero
 Servo speedControl, steerControl; //Create as much as Servoobject you want. You can controll 2 or more Servos at the same time
 
-const int forward = 1380;//1380;
-const int stop_val = 1200;
-const int reverse = 1100;
-//const int right = ;
+const int forward = 1602; // 1549: lowest speed
+const int stop_val = 1520; //1200: stop
+const int reverse = 1400;
 
-void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+const int right = 1300;
+const int straight = 1667;
+const int left = 1800;
+
+void setup(){
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+  pinMode(trigPin5, OUTPUT);
+  pinMode(echoPin5, INPUT);
   
-  speedControl.attach(11); // SPEED
-  steerControl.attach(12); // STEER
+  speedControl.attach(speedPin);
+  steerControl.attach(steerPin);
   Serial.begin(9600);
 }
 
 void loop() {
-  int dist = getDistance();
-  //printf("%f\n",dist);
-  Serial.print("\n");
-  Serial.print(dist);
-  if(dist < 15){
+  int dist3 = getDistance(trigPin3, echoPin3);
+  int dist5 = getDistance(trigPin5, echoPin5);
+  Serial.print("\ndist3:");
+  Serial.print(dist3);
+  Serial.print("\ndist5:");
+  Serial.print(dist5);
+  if(dist5 <= 15){
     stop_car();
+    turn_left();
   }
   else{
     forward_car();
+    go_straight();
   }
+  
 }
-
-int getDistance(){
+int getDistance(int trigPin, int echoPin){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -42,8 +56,8 @@ int getDistance(){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
   return distance;
 }
 
@@ -56,10 +70,13 @@ void forward_car(){
 }
 
 void turn_right(){
-//  steerControl.writeMicroseconds();
+  steerControl.writeMicroseconds(right);
 }
 
 void turn_left(){
-  //steerControl.writeMicroseconds();
+  steerControl.writeMicroseconds(left);
 }
 
+void go_straight(){
+  steerControl.writeMicroseconds(straight);
+}
